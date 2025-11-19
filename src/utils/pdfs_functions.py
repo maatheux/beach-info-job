786235d377797062da20babdaaf92489ab8d62b3
maatheuxlinux <matheusmarinho.lima@hotmail.com>
2025-11-19 11:14:15 -0300

    fix: DIR_PATH in download and remove pdfs

--- src/utils/pdfs_functions.py
+++ src/utils/pdfs_functions.py
@@ -1,7 +1,9 @@
 import os
 import shutil
-
 import pdfplumber
+import requests
+
+from src.config.paths import DATA_DIR
 
 
 def read_pdfs(file_path):
@@ -23,7 +25,7 @@ def read_pdfs(file_path):
 
 
 def remove_pdfs():
-    pdf_dir = 'data/pdfs'
+    pdf_dir = f'{DATA_DIR}/pdfs'
 
     for name in os.listdir(pdf_dir):
         path = os.path.join(pdf_dir, name)
@@ -32,3 +34,26 @@ def remove_pdfs():
             os.remove(path)
         elif os.path.isdir(path):
             shutil.rmtree(path)
+
+
+def download_file(url: str, filename: str) -> str | None:
+    try:
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/122.0.0.0 Safari/537.36"
+        }
+
+        resposta = requests.get(url, headers=headers)
+
+        if resposta.status_code == 200:
+            with open(filename, "wb") as f:
+                f.write(resposta.content)
+
+            return filename
+
+        else:
+            return None
+
+    except Exception as e:
+        return None
